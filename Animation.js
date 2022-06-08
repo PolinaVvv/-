b7e8b59b556235e14b717d3c7699fd78c16973a9
PolinaVvv <zipseal@yandex.ru>
2022-06-09 01:18:55 +0300

    Доделала сердечко

--- Animation.js
+++ Animation.js
@@ -1,11 +1,50 @@
+// "выплывашка" по id одно элемента
+// let element = document.getElementById("para1");
+// element.onclick = change;
 
-let element = document.getElementById("para1");
-element.onclick = change;
+// function change(){
+//     console.log("hi");
+//     element.style.transition = "all 2s";
+//     element.style.opacity = 1;
+//     setTimeout("element.style.opacity = 0", 1500);
+// }
 
-function change(){
-    console.log("hi");
-    element.style.transition = "all 2s";
-    element.style.opacity = 1;
-    setTimeout("element.style.opacity = 0", 1500);
+// "выплывашка" с помощью класса, но без полупрозрачной наводки
+// const element = document.getElementsByClassName("position-and-size")
+// const arrayID = [...element]
+// for (let el of arrayID) {
+//     el.onclick = change
+//     console.log(el)
+
+//     function change(){
+//         console.log("hi");
+//         el.style.transition = "all 2s"
+//         el.style.opacity = 1
+//         setTimeout(reverseChange, 1000)
+//     }
+//     function reverseChange(){
+//         el.style.opacity = 0
+//     }
+// }
+
+// "выплывашка" с помощью класса с полупрозрачной наводкой
+const element = document.getElementsByClassName("position-and-size")
+const arrayID = [...element]
+for (let el of arrayID) {
+    el.onmouseover = translucency
+    el.onmouseout = reverseChange
+    el.onclick = change
+
+    function translucency() {
+        el.style.opacity = 0.5
+    }
+    function change(){
+        console.log("hi");
+        el.style.transition = "all 2s"
+        el.style.opacity = 1
+        setTimeout(reverseChange, 1000)
+    }
+    function reverseChange(){
+        el.style.opacity = 0
+    }
 }
-
--- Task3.js
+++ Task3.js
@@ -1,66 +1,81 @@
-let user = {
-    name: "John",
-};
-alert(user.name);
+// let user = {
+//     name: "John",
+// };
+// alert(user.name);
 
-user.surname = "Smith";
-alert(user.surname);
+// user.surname = "Smith";
+// alert(user.surname);
 
-user.name = "Pete";
-alert(user.name);
+// user.name = "Pete";
+// alert(user.name);
 
-delete user.name;
-alert(user.name);
-
-
-function isEmpty(obj){
-    for (let key in obj){
-        return false;
-    }
-    return true;
-}
-
-let schedule = {};
-alert( isEmpty(schedule) ); // true
-
-schedule["8:30"] = "get up";
-alert( isEmpty(schedule) ); // false
-alert(schedule["8:30"]);
+// delete user.name;
+// alert(user.name);
 
 
-function count(obj){
-    let sum = 0;
-    for (let key in obj){
-        sum += obj[key];
-    }
-    return sum;
-}
+// function isEmpty(obj){
+//     for (let key in obj){
+//         return false;
+//     }
+//     return true;
+// }
 
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-  };
+// let schedule = {};
+// alert( isEmpty(schedule) ); // true
 
-alert(count(salaries));
+// schedule["8:30"] = "get up";
+// alert( isEmpty(schedule) ); // false
+// alert(schedule["8:30"]);
 
 
+// function count(obj){
+//     let sum = 0;
+//     for (let key in obj){
+//         sum += obj[key];
+//     }
+//     return sum;
+// }
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   };
+
+// alert(count(salaries));
+
+// идея - просто рекурсивно вызывать ф-цию, передавая новый объект (пыталась, но очень хочу спать, простите)
 function multiplyNumeric(obj){
     for (let key in obj){
         if (typeof obj[key] == "number"){
             obj[key] *= 2;
         }
+        else if (typeof obj[key] == "object"){
+            multiplyNumeric(obj[key])
+        }
     }
+}
+
+function printMenu(menu){
+    for (let key in menu){
+        if (typeof obj[key] == "object"){
+            printMenu(obj[key])
+        }
+        alert(menu[key]);
+}
 }
 
 let menu = {
     width: 200,
     height: 300,
-    title: "My menu"
+    title: "My menu",
+    menu2: {
+        width: 400,
+        height: 600,
+        title: "My menu",
+      }
   };
   
 multiplyNumeric(menu);
+printMenu(menu);
 
-for (let key in menu){
-    alert(menu[key]);
-}

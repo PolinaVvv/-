b9066fa2ef7dd20447106f9ea9a737ded8ac142a
Polina <78511103+PolinaVvv@users.noreply.github.com>
2022-06-14 16:02:02 +0300

    fetch

--- FormProcessing.js
+++ FormProcessing.js
@@ -1,9 +1,33 @@
 const element = document.getElementById('form1')
 
-/* element.onsubmit = function (evt) {
-  // событие onsubmit возникает при клике
+element.onsubmit = async function (evt) {
   evt.preventDefault() // отмена авт отправки
-  const email = document.getElementById('email').value
+  const url = 'http://localhost/test'
+  // fetch(url, []).then(() => console.log('hi'))
+  let formData = {
+    surname: document.getElementById('surname').value,
+    email: document.getElementById('email').value,
+    name: document.getElementById('name').value,
+    phone: document.getElementById('phone').value,
+    series: document.getElementById('series').value,
+    number: document.getElementById('number').value,
+    checkbox: document.getElementById('checkbox').checked
+      ? (ch = 'yes')
+      : (ch = 'no'),
+  }
+
+  let response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+
+  let result = await response.json()
+  alert(JSON.stringify(result))
+
+  /*  const email = document.getElementById('email').value
   const password = document.getElementById('password').value
   const text = document.getElementById('text').value
   const checkbox = document.getElementById('checkbox')
@@ -11,5 +35,5 @@ const element = document.getElementById('form1')
   checkbox.checked ? (ch = 'yes') : (ch = 'no')
   alert(
     `Ваш email: ${email}\nВаш пароль: ${password}\nВаши серия и номер паспорта: ${text}\nВаш ответ на оформление кредита: ${ch}`
-  )
-} */
+  ) */
+}
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 80
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Test')
 })
 
-app.get('/test', (req, res) => {
-  console.log(req.query)
+app.post('/test', (req, res) => {
+  console.log(req.body)
   // res.send(JSON.stringify(req.query))
-  res.send(
-    `На Ваши ИФ ${req.query.name} ${req.query.surname} и паспортный данные ${req.query.series} ${req.query.number} оформлен кредит на сумму 500000000000$. А также бонусом на почту ${req.query.email} будет прилетать спам.Хорошего дня!`
+  console.log(
+    `На Ваши ИФ ${req.body.name} ${req.body.surname} и паспортный данные ${req.body.series} ${req.body.number} оформлен кредит на сумму 500000000000$. А также бонусом на почту ${req.body.email} будет прилетать спам.Хорошего дня!`
   )
+  res.send(JSON.stringify(req.body.series))
 })
 
 app.listen(port, () => {
